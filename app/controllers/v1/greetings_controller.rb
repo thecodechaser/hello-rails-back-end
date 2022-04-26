aa47aa155adb2729a5465796c7da4d256034fa0f
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-26 13:48:06 +0530

    Create controller

---
+++ app/controllers/v1/greetings_controller.rb
@@ -0,0 +1,10 @@
+class V1::GreetingsController < ApplicationController
+  def index
+    @greeting = Greeting.order('RANDOM()').first
+    if @greeting
+      render json: { success: true, message: 'Loaded greeting message', data: { greeting: @greeting } }, status: :ok
+    else
+      render json: { success: false, errors: 'Oops, Something went wrong!' }, status: :unprocessable_entity
+    end
+  end
+end
