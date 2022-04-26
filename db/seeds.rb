56d453de6556801c49222f75947663a12096956d
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-26 14:19:25 +0530

    Add data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(:message => "HI!  Welcome to Rails React App!!!")
+Greeting.create(:message => "HI!  Greeting from the App!!!")
+Greeting.create(:message => "HI!  How are you today?!!!")
+Greeting.create(:message => "HI!  Trust me, This is very cool App!!!")
+Greeting.create(:message => "HI!  Haha, Again me welcoming you!!!")
